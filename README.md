96015335da83c2c2946c3fc3dc1709fbe68a5a35
DreWulff <awulff98@gmail.com>
2024-07-14 00:15:31 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# DiscordMessageChomper
-Discord bot written in Python made only to eat a channel's messages one by one.
+# Discord Message Chomper
+## Description
+A Discord bot written in Python made only to eat a channel's messages one by one.  
+Commands are explained in the **Commands** section.
+
+**Be careful when setting up the bot's permissions and channels as it will delete ALL messages one by one, or until stopped.**
+
+## Setup
+Clone this repository with the following command:
+
+    git clone https://github.com/DreWulff/DiscordMessageChomper
+
+Make sure all libraries/modules required are installed.
+
+Create a .env file with the next lines, replacing the values in brackets:
+
+    #.env
+    DISCORD_TOKEN=[Token from your Discord bot]
+    DISCORD_GUILD=[Name of Discord server]
+
+## Run
+To start the bot execute the command:
+
+    python bot.py
+
+## Commands
+* **/chomp [rate=1]**:
+  * Starts deleting messages from newest to oldest at desired rate.
+  * Rate of deleted messages will stay in the range between 1 and 3 messages per second.
+
+* **/shut**:
+  * Stops the deletion of messages.
---
+++ bot.py
@@ -0,0 +1,68 @@
+# bot.py
+# Python libraries
+import os
+import asyncio
+from datetime import datetime, timedelta, timezone
+
+# External libraries
+from dotenv import load_dotenv
+import discord
+from discord import app_commands
+from discord.ext import commands, tasks
+from discord.app_commands import Choice
+
+# .env variables
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Variables for bot initialization
+INTENTS = discord.Intents.default()
+INTENTS.message_content = True
+BOT = commands.Bot(command_prefix="!", intents=INTENTS)
+CHOMPING=False
+
+@BOT.event
+async def on_ready():
+    try:
+        synced = await BOT.tree.sync()
+        print(f"Synced {len(synced)} commands(s).")
+    except Exception as exception:
+        print(exception)
+
+@app_commands.describe(rate="Chomping rate [chomps/s]. Min: 1; Max: 3")
+@BOT.tree.command(name="chomp", description="Chomps messages at desired rate [chomps/s].")
+async def chomp(interaction: discord.Interaction, rate: int = 1):
+    global CHOMPING
+    print("Chomping...")
+    now = datetime.now()
+    rate = 3 if rate > 3 else rate
+    rate = 1 if rate < 1 else rate
+    async for guild in BOT.fetch_guilds(limit=None):
+        channel = [channel for channel in await guild.fetch_channels() if channel.name == "cave-of-whimsy"][0]
+        try:
+            await interaction.response.send_message("**OM NOM NOM NOM NOM!**")
+            CHOMPING=True
+            async for message in channel.history(limit=None, oldest_first=False):
+                if (not CHOMPING): break
+                if ((datetime.utcnow().replace(tzinfo=None) - message.created_at.replace(tzinfo=None) > timedelta(minutes=1)) or (message.author != BOT.user)):
+                # Check if the message is older than 24 hours
+                #if now - message.created_at > timedelta(hours=1):
+                    print("Chomped! -> " + message.content)
+                    await message.delete()
+                    await asyncio.sleep(1.0/rate)  # Avoid rate limits
+        except discord.Forbidden:
+            print(f"Permission error in channel {channel.name}")
+        except discord.HTTPException as e:
+            print(f"HTTP error in channel {channel.name}: {e}")
+        CHOMPING=False
+
+@BOT.tree.command(name="shut", description="Stops the CHOMP.")
+async def shut(interaction: discord.Interaction):
+    global CHOMPING
+    if (not CHOMPING):
+        await interaction.response.send_message("**???**", ephemeral=True)
+    else:
+        CHOMPING=False
+        await interaction.response.send_message("**MMMM DELICIOUS**", ephemeral=True)
+
+BOT.run(TOKEN)
