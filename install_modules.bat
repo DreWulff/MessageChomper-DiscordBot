dec0b824eec50656500a25b12516fd7e086464b1
DreWulff <awulff98@gmail.com>
2025-07-16 23:41:08 -0400

    Simplified module script

--- install_modules.bat
+++ install_modules.bat
@@ -1,10 +1,1 @@
-@echo off
-REM Create virtual environment
-python -m venv venv
-call venv\Scripts\activate
-
-REM Upgrade pip
-python -m pip install --upgrade pip
-
-REM Install external dependencies
-pip install python-dotenv discord.py
+call pip install python-dotenv discord.py
